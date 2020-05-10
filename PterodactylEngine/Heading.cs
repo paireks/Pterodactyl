7e5fd5031bc6265e53222484afd5dcf42e816250
W Radaczyński <w.radaczynski@gmail.com>
2020-05-10 21:47:53 +0200

    Heading

---
+++ PterodactylEngine/Heading.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PterodactylEngine
+{
+    public class Heading
+    {
+        private string _text;
+        private int _level;
+
+        public Heading(string text, int level)
+        {
+            Text = text;
+            Level = level;
+        }
+
+        public string Create()
+        {
+            string levelHash = "";
+            for (int i = 0; i < Level; i++)
+            {
+                levelHash += "#";
+            }
+            string reportPart = levelHash + " " + Text;
+
+            return reportPart;
+        }
+
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+        public int Level
+        {
+            get { return _level; }
+            set {
+                if (new List<int> { 1, 2, 3, 4, 5, 6 }.Contains(value) )
+                {
+                    _level = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Level should be an integer between 1 and 6");
+                }
+            }
+        }
+    }
+}
---
+++ UnitTestEngine/TestHeading.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using PterodactylEngine;
+using Xunit;
+
+namespace UnitTestEngine
+{
+    public class TestHeadingHelper : TheoryData<string, int, string>
+    {
+        public TestHeadingHelper()
+        {
+            Add("Lol nice heading tho", 1, "# Lol nice heading tho");
+            Add("Different level", 2, "## Different level");
+            Add("Last level", 6, "###### Last level");
+            Add("Multiline check" + Environment.NewLine + "Hmm", 3, "### Multiline check"+ Environment.NewLine + "Hmm");
+        }
+    }
+
+    public class TestHeading
+    {
+        [Theory]
+        [ClassData(typeof(TestHeadingHelper))]
+        public void CorrectData(string text, int level, string expected)
+        {
+            Heading testObject = new Heading(text, level);
+            Assert.Equal(text, testObject.Text);
+        }
+
+        [Theory]
+        [ClassData(typeof(TestHeadingHelper))]
+        public void CheckReportCreation(string text, int level, string expected)
+        {
+            Heading testObject = new Heading(text, level);
+            string actual = testObject.Create();
+
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
