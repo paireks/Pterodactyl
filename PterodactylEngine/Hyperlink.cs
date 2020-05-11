40d1521461dc36e7b5d907c27a0d4f1b7c5e8102
W Radaczyński <w.radaczynski@gmail.com>
2020-05-11 13:06:54 +0200

    Hyperlink

---
+++ PterodactylEngine/Hyperlink.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PterodactylEngine
+{
+    public class Hyperlink
+    {
+        private string _text;
+        private string _link;
+
+        public Hyperlink(string text, string link)
+        {
+            Text = text;
+            Link = link;
+        }
+
+        public string Create()
+        {
+            string reportPart = "[" + Text + "](" + Link + ")";
+
+            return reportPart;
+        }
+
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+        public string Link
+        {
+            get { return _link; }
+            set { _link = value; }
+        }
+    }
+}
+
