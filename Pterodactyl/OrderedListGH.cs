ff2ae76e8f43276bdec557ba8d8bcd7002e03687
W Radaczyński <w.radaczynski@gmail.com>
2020-05-12 17:44:08 +0200

    Ordered List

---
+++ Pterodactyl/OrderedListGH.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using PterodactylEngine;
+
+namespace Pterodactyl
+{
+    public class OrderedListGH : GH_Component
+    {
+        public OrderedListGH()
+          : base("Ordered List", "Ordered List",
+              "Add ordered list",
+              "Pterodactyl", "Parts")
+        {
+        }
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddTextParameter("Items", "Items", "Different list items as text list",
+                  GH_ParamAccess.list, new List<string> { "First element list",
+                  "Second element list"});
+        }
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Report Part", "Report Part", "Created part of the report", GH_ParamAccess.item);
+        }
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            List<string> text = new List<string>();
+
+            DA.GetDataList(0, text);
+
+            string report;
+            OrderedList taskObject = new OrderedList(text);
+            report = taskObject.Create();
+
+            DA.SetData(0, report);
+        }
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return null;
+            }
+        }
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("16078687-ab1b-4e4e-a71e-1877c40ce5f7"); }
+        }
+    }
+}
