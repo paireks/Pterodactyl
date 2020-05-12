4b292e432bb8ba4268b3c2ad47d9aa92b3affa09
W Radaczyński <w.radaczynski@gmail.com>
2020-05-12 02:52:15 +0200

    Task list

---
+++ PterodactylEngine/TaskList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PterodactylEngine
+{
+    public class TaskList
+    {
+        private List<string> _text;
+        private List<bool> _done;
+
+        public TaskList(List<string> text, List<bool> done)
+        {
+            Text = text;
+            Done = done;
+
+            if (Text.Count != Done.Count) 
+            {
+                throw new ArgumentException("Text values should match boolean (done) values." +
+                    "Check if both input lists have the same number of elements.");
+            }
+        }
+
+        public string Create()
+        {
+            string reportPart = "";
+            for (int i = 0; i < Text.Count; i++)
+            {
+                if (Done[i] == true) { reportPart += "- [x] "; }
+                else { reportPart += "- [ ] "; }
+                reportPart += Text[i];
+                if (i+1 != Text.Count) { reportPart += Environment.NewLine; }
+            }
+            return reportPart;
+        }
+
+        public List<string> Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+        public List<bool> Done
+        {
+            get { return _done; }
+            set { _done = value; }
+        }
+    }
+}
---
+++ UnitTests/TestTaskListGH.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Grasshopper.Kernel;
+using Pterodactyl;
+using Xunit;
+
+namespace UnitTestsGH
+{
+    public class TestTaskListGhHelper
+    {
+        public static TaskListGH TestObject
+        {
+            get
+            {
+                TaskListGH testObject = new TaskListGH();
+                return testObject;
+            }
+        }
+    }
+    public class TestTaskListGh
+    {
+        [Theory]
+        [InlineData("Task List", "Task List",
+            "Add task list",
+            "Pterodactyl", "Parts")]
+        public void TestName(string name, string nickname, string description, string category, string subCategory)
+        {
+            Assert.Equal(name, TestTaskListGhHelper.TestObject.Name);
+            Assert.Equal(nickname, TestTaskListGhHelper.TestObject.NickName);
+            Assert.Equal(description, TestTaskListGhHelper.TestObject.Description);
+            Assert.Equal(category, TestTaskListGhHelper.TestObject.Category);
+            Assert.Equal(subCategory, TestTaskListGhHelper.TestObject.SubCategory);
+        }
+
+        [Theory]
+        [InlineData(0, "Tasks", "Tasks", "Different tasks as text list", GH_ParamAccess.list)]
+        [InlineData(1, "Done", "Done", "Boolean list. True = done.", GH_ParamAccess.list)]
+        public void TestRegisterInputParams(int id, string name, string nickname,
+            string description, GH_ParamAccess access)
+        {
+            Assert.Equal(name, TestTaskListGhHelper.TestObject.Params.Input[id].Name);
+            Assert.Equal(nickname, TestTaskListGhHelper.TestObject.Params.Input[id].NickName);
+            Assert.Equal(description, TestTaskListGhHelper.TestObject.Params.Input[id].Description);
+            Assert.Equal(access, TestTaskListGhHelper.TestObject.Params.Input[id].Access);
+        }
+
+        [Theory]
+        [InlineData(0, "Report Part", "Report Part", "Created part of the report", GH_ParamAccess.item)]
+        public void TestRegisterOutputParams(int id, string name, string nickname,
+            string description, GH_ParamAccess access)
+        {
+            Assert.Equal(name, TestTaskListGhHelper.TestObject.Params.Output[id].Name);
+            Assert.Equal(nickname, TestTaskListGhHelper.TestObject.Params.Output[id].NickName);
+            Assert.Equal(description, TestTaskListGhHelper.TestObject.Params.Output[id].Description);
+            Assert.Equal(access, TestTaskListGhHelper.TestObject.Params.Output[id].Access);
+        }
+
+        [Fact]
+        public void TestGuid()
+        {
+            Guid expected = new Guid("6d67b19c-bd15-44eb-9524-e0856ff55d1b");
+            Guid actual = TestTaskListGhHelper.TestObject.ComponentGuid;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
