917c753ee28a1d98f8d5b27dbacfc8f79841db05
W Radaczyński <w.radaczynski@gmail.com>
2020-05-17 19:10:12 +0200

    GH new components tests

---
+++ UnitTests/TestHorizontalLineGH.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Grasshopper.Kernel;
+using Pterodactyl;
+using Xunit;
+
+namespace UnitTestsGH
+{
+    public class TestHorizontalLineGhHelper
+    {
+        public static HorizontalLineGH TestObject
+        {
+            get
+            {
+                HorizontalLineGH testObject = new HorizontalLineGH();
+                return testObject;
+            }
+        }
+    }
+    public class TestHorizontalLineGh
+    {
+        [Theory]
+        [InlineData("Horizontal Line", "Horizontal Line",
+              "Add horizontal line",
+              "Pterodactyl", "Parts")]
+        public void TestName(string name, string nickname, string description, string category, string subCategory)
+        {
+            Assert.Equal(name, TestHorizontalLineGhHelper.TestObject.Name);
+            Assert.Equal(nickname, TestHorizontalLineGhHelper.TestObject.NickName);
+            Assert.Equal(description, TestHorizontalLineGhHelper.TestObject.Description);
+            Assert.Equal(category, TestHorizontalLineGhHelper.TestObject.Category);
+            Assert.Equal(subCategory, TestHorizontalLineGhHelper.TestObject.SubCategory);
+        }
+
+        [Theory]
+        [InlineData(0, "Report Part", "Report Part", "Created part of the report", GH_ParamAccess.item)]
+        public void TestRegisterOutputParams(int id, string name, string nickname,
+            string description, GH_ParamAccess access)
+        {
+            Assert.Equal(name, TestHorizontalLineGhHelper.TestObject.Params.Output[id].Name);
+            Assert.Equal(nickname, TestHorizontalLineGhHelper.TestObject.Params.Output[id].NickName);
+            Assert.Equal(description, TestHorizontalLineGhHelper.TestObject.Params.Output[id].Description);
+            Assert.Equal(access, TestHorizontalLineGhHelper.TestObject.Params.Output[id].Access);
+        }
+
+        [Fact]
+        public void TestGuid()
+        {
+            Guid expected = new Guid("db6a6683-e51e-4a89-bb5e-3cdea64bda4e");
+            Guid actual = TestHorizontalLineGhHelper.TestObject.ComponentGuid;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ UnitTests/TestUnderlineGHcs.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Grasshopper.Kernel;
+using Pterodactyl;
+using Xunit;
+
+namespace UnitTestsGH
+{
+    public class TestUnderlineGhHelper
+    {
+        public static UnderlineGH TestObject
+        {
+            get
+            {
+                UnderlineGH testObject = new UnderlineGH();
+                return testObject;
+            }
+        }
+    }
+    public class TestUnderlineGh
+    {
+        [Theory]
+        [InlineData("Underline", "Underline",
+              "Format text -> underline",
+              "Pterodactyl", "Format")]
+        public void TestName(string name, string nickname, string description, string category, string subCategory)
+        {
+            Assert.Equal(name, TestUnderlineGhHelper.TestObject.Name);
+            Assert.Equal(nickname, TestUnderlineGhHelper.TestObject.NickName);
+            Assert.Equal(description, TestUnderlineGhHelper.TestObject.Description);
+            Assert.Equal(category, TestUnderlineGhHelper.TestObject.Category);
+            Assert.Equal(subCategory, TestUnderlineGhHelper.TestObject.SubCategory);
+        }
+
+        [Theory]
+        [InlineData(0, "Text", "Text", "Text to format", GH_ParamAccess.item)]
+        public void TestRegisterInputParams(int id, string name, string nickname,
+            string description, GH_ParamAccess access)
+        {
+            Assert.Equal(name, TestUnderlineGhHelper.TestObject.Params.Input[id].Name);
+            Assert.Equal(nickname, TestUnderlineGhHelper.TestObject.Params.Input[id].NickName);
+            Assert.Equal(description, TestUnderlineGhHelper.TestObject.Params.Input[id].Description);
+            Assert.Equal(access, TestUnderlineGhHelper.TestObject.Params.Input[id].Access);
+        }
+
+        [Theory]
+        [InlineData(0, "Report Part", "Report Part", "Created part of the report", GH_ParamAccess.item)]
+        public void TestRegisterOutputParams(int id, string name, string nickname,
+            string description, GH_ParamAccess access)
+        {
+            Assert.Equal(name, TestUnderlineGhHelper.TestObject.Params.Output[id].Name);
+            Assert.Equal(nickname, TestUnderlineGhHelper.TestObject.Params.Output[id].NickName);
+            Assert.Equal(description, TestUnderlineGhHelper.TestObject.Params.Output[id].Description);
+            Assert.Equal(access, TestUnderlineGhHelper.TestObject.Params.Output[id].Access);
+        }
+
+        [Fact]
+        public void TestGuid()
+        {
+            Guid expected = new Guid("1386bcfa-a262-429d-ab64-55b36b18ca2a");
+            Guid actual = TestUnderlineGhHelper.TestObject.ComponentGuid;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
