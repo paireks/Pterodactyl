577106377962fef3e3d18e0b1c5890825cbcdb4c
W Radaczyński <w.radaczynski@gmail.com>
2020-05-17 18:05:26 +0200

    Table complete

--- Pterodactyl/TableGH.cs
+++ Pterodactyl/TableGH.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 using Grasshopper.Kernel;
+using PterodactylEngine;
 
 namespace Pterodactyl
 {
@@ -16,16 +17,52 @@ namespace Pterodactyl
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
             pManager.AddTextParameter("Table Headings", "Table Headings", "Headings inside table as text list", GH_ParamAccess.list);
-            pManager.AddNumberParameter("Alignment", "Alignment", "Alignment for each column as matching integer list. 0 = left, 1 = center, 2 = right", GH_ParamAccess.list);
+            pManager.AddIntegerParameter("Alignment", "Alignment", "Alignment for each column as matching integer list. 0 = left, 1 = center, 2 = right", GH_ParamAccess.list);
             pManager.AddTextParameter("Data Tree", "Data Tree", "Data as tree. Each branch is treated as one column." +
                 " Inside each branch should be a list of text elements, elements represent rows inside that column.", GH_ParamAccess.tree);
         }
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-            pManager.AddTextParameter("Report Part", "Report Part", "Created part of the report", GH_ParamAccess.item);
+            pManager.AddTextParameter("Report Part", "Report Part", "Created part of the report", GH_ParamAccess.list);
         }
         protected override void SolveInstance(IGH_DataAccess DA)
         {
+            List<string> headings = new List<string>();
+            List<int> alignment = new List<int>();
+            Grasshopper.Kernel.Data.GH_Structure<Grasshopper.Kernel.Types.GH_String> dataTree = new Grasshopper.Kernel.Data.GH_Structure<Grasshopper.Kernel.Types.GH_String>();
+            
+            DA.GetDataList(0, headings);
+            DA.GetDataList(1, alignment);
+            DA.GetDataTree("Data Tree", out dataTree);
+
+            int currentColumnHeight = 1;
+
+            for (int i = 0; i < dataTree.Branches.Count; i++) //for each column check for highest
+            {
+                if (dataTree.get_Branch(i).Count > currentColumnHeight)
+                {
+                    currentColumnHeight = dataTree.get_Branch(i).Count;
+                }
+            }
+
+            string[,] dataTreeArray = new string[dataTree.Branches.Count, currentColumnHeight];
+
+            for (int i = 0; i < dataTree.Branches.Count; i++) //for each column
+            {
+                for (int j = 0; j < dataTree.get_Branch(i).Count; j++)
+                {
+                    dataTreeArray[i, j] = dataTree.get_Branch(i)[j].ToString();
+                }
+            }
+            
+
+            List<string> report;
+            Table reportObject = new Table(headings, alignment, dataTreeArray);
+            report = reportObject.Create();
+
+            DA.SetDataList(0, report);
+
+
         }
         protected override System.Drawing.Bitmap Icon
         {
