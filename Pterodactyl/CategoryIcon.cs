855899e43e8a098c899cb71dd6de5d97746a9815
Praneet Mathur <contact@praneetmathur.me>
2020-10-15 15:44:25 +0530

    Added category icon

---
+++ Pterodactyl/CategoryIcon.cs
@@ -0,0 +1,19 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pterodactyl
+{
+    class CategoryIcon : GH_AssemblyPriority
+    {
+        public override GH_LoadingInstruction PriorityLoad()
+        {
+            Instances.ComponentServer.AddCategoryIcon("Pterodactyl", Properties.Resources.tab_icon);
+            return GH_LoadingInstruction.Proceed;
+        }
+    }
+}
--- Pterodactyl/Properties/Resources.Designer.cs
+++ Pterodactyl/Properties/Resources.Designer.cs
@@ -473,6 +473,16 @@ namespace Pterodactyl.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap tab_icon {
+            get {
+                object obj = ResourceManager.GetObject("tab_icon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap temp {
             get {
                 object obj = ResourceManager.GetObject("temp", resourceCulture);
