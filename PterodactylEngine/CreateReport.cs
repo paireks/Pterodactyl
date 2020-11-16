a44bf12cb54f5f6d678c632ef886f89379f980e9
Praneet Mathur <contact@praneetmathur.me>
2020-11-16 18:51:56 +0530

    Update CreateReport.cs

--- PterodactylEngine/CreateReport.cs
+++ PterodactylEngine/CreateReport.cs
@@ -6,30 +6,27 @@ namespace PterodactylEngine
 {
     public class CreateReport
     {
-        private List<string> _reportParts;
-        private List<PterodactylGrasshopperBitmapGoo> _assets;
+        private List<object> _reportParts;
         private string _title;
         private bool _tableOfContents;
 
 
         public CreateReport(List<object> reportParts, string title, bool tableOfContents)
         {
-            List<string> textParts = new List<string>();
-            List<PterodactylGrasshopperBitmapGoo> assetParts = new List<PterodactylGrasshopperBitmapGoo>();
+            List<object> rParts = new List<object>();
             foreach (object part in reportParts)
             {
                 if (part is Grasshopper.Kernel.Types.GH_String)
                 {
-                    textParts.Add((part as Grasshopper.Kernel.Types.GH_String).Value);
+                    rParts.Add((part as Grasshopper.Kernel.Types.GH_String).Value);
                 }
                 else if (part is PterodactylGrasshopperBitmapGoo)
                 {
-                    assetParts.Add(part as PterodactylGrasshopperBitmapGoo);
+                    rParts.Add(part as PterodactylGrasshopperBitmapGoo);
                 }
                 else throw new InvalidCastException("Report Parts must be either text parts or image assets");
             }
-            ReportParts = textParts;
-            Assets = assetParts;
+            ReportParts = rParts;
             TableOfContents = tableOfContents;
             Title = title;
         }
@@ -49,31 +46,28 @@ namespace PterodactylEngine
                 report += "[TOC]" + Environment.NewLine;
             }
 
-            foreach (string part in ReportParts)
+            foreach (object part in ReportParts)
             {
-                report += part + Environment.NewLine;
-            }
-
-            foreach (PterodactylGrasshopperBitmapGoo asset in Assets)
-            {
-                markdown.AddAsset(asset);
-                report += asset.ReportPart + Environment.NewLine;
+                if (part is string)
+                {
+                    report += (part as string) + Environment.NewLine;
+                }
+                else if (part is PterodactylGrasshopperBitmapGoo)
+                {
+                    markdown.AddAsset(part as PterodactylGrasshopperBitmapGoo);
+                    report += (part as PterodactylGrasshopperBitmapGoo).ReportPart + Environment.NewLine;
+                }
+                else throw new InvalidCastException("Invalid Report Part Object");
             }
             markdown.Markup = report;
 
             return markdown;
         }
 
-        public List<string> ReportParts
+        public List<object> ReportParts
         {
             get { return _reportParts; }
             set { _reportParts = value; }
-        }
-
-        public List<PterodactylGrasshopperBitmapGoo> Assets
-        {
-            get { return _assets; }
-            set { _assets = value; }
         }
 
         public string Title
