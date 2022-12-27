80d61f3cb52c08f01687c7af5bd8a204200911ef
paireks <w.radaczynski@gmail.com>
2022-12-27 21:29:42 +0100

    TestGraphLegend.cs

--- PterodactylCharts/GraphLegend.cs
+++ PterodactylCharts/GraphLegend.cs
@@ -10,53 +10,51 @@ namespace PterodactylCharts
             Title = title;
             Position = position;
 
-            if (Position > 12 || Position < 0)
+            if (Position > 11 || Position < 0)
             {
-                throw new ArgumentException("Position should be between 0 and 12");
+                throw new ArgumentException("Position should be between 0 and 11");
             }
         }
         
-        public GraphLegend(string title, int position, int placement, int orientation, double size)
+        public GraphLegend(string title, int position, int placement, int orientation, double textSize)
         {
             Title = title;
             Position = position;
             Placement = placement;
             Orientation = orientation;
-            TextSize = size;
+            TextSize = textSize;
 
 
             if (Position > 11 || Position < 0)
             {
                 throw new ArgumentException("Position should be between 0 and 11");
             }
-
-            if (Orientation > 1 || Orientation < 0)
-            {
-                throw new ArgumentException("Orientation should be between 0 and 1");
-            }
-
             if (Placement > 1 || Placement < 0)
             {
                 throw new ArgumentException("Placement should be between 0 and 1");
             }
+            if (Orientation > 1 || Orientation < 0)
+            {
+                throw new ArgumentException("Orientation should be between 0 and 1");
+            }
             if (TextSize > 140 || TextSize < 5)
             {
-                throw new ArgumentException("Text size 140 <> 5");
+                throw new ArgumentException("Text size should be between 5 and 140");
             }
         }
 
         public override string ToString()
         {
             StringBuilder stringRepresentation = new StringBuilder();
-            stringRepresentation.AppendFormat("Graph Legend{0}Title: {1}{0}Position: {2}", Environment.NewLine, Title, Position, Orientation, Placement);
+            stringRepresentation.AppendFormat("Graph Legend{0}Title: {1}", Environment.NewLine, Title);
             return stringRepresentation.ToString();
         }
 
-        public string Title { get; set; }
-        public int Position { get; set; }
-        public int Placement { get; set; }
-        public int Orientation { get; set; }
-        public double TextSize { get; set; }
+        public string Title { get; }
+        public int Position { get; }
+        public int Placement { get; }
+        public int Orientation { get; }
+        public double TextSize { get; }
 
     }
 }
--- PterodactylChartsTests/TestGraphLegend.cs
+++ PterodactylChartsTests/TestGraphLegend.cs
@@ -11,8 +11,8 @@ namespace UnitTestEngine
     {
         public TestGraphLegendHelper()
         {
-            Add("Title example", 0, "Graph Legend" + Environment.NewLine + "Title: Title example"+ Environment.NewLine + "Position: 0");
-            Add("Title example", 12, "Graph Legend" + Environment.NewLine + "Title: Title example"+ Environment.NewLine + "Position: 12");
+            Add("Title example", 0, "Graph Legend" + Environment.NewLine + "Title: Title example");
+            Add("Title example", 11, "Graph Legend" + Environment.NewLine + "Title: Title example");
         }
     }
 
@@ -20,10 +20,35 @@ namespace UnitTestEngine
     {
         public TestGraphLegendExceptionHelper()
         {
-            Add("Title example", -1, "Position should be between 0 and 12");
-            Add("Title example", 13, "Position should be between 0 and 12");
+            Add("Title example", -1, "Position should be between 0 and 11");
+            Add("Title example", 12, "Position should be between 0 and 11");
         }
     }
+    
+    public class TestGraphLegendAdvancedHelper : TheoryData<string, int, int, int, double, string>
+    {
+        public TestGraphLegendAdvancedHelper()
+        {
+            Add("Title example", 0, 0, 0, 5.0, "Graph Legend" + Environment.NewLine + "Title: Title example");
+            Add("Title example", 11, 1, 1, 140.0, "Graph Legend" + Environment.NewLine + "Title: Title example");
+        }
+    }
+
+    public class TestGraphLegendAdvancedExceptionHelper : TheoryData<string, int, int, int, double, string>
+    {
+        public TestGraphLegendAdvancedExceptionHelper()
+        {
+            Add("Title example", -1, 0, 0, 5.0, "Position should be between 0 and 11");
+            Add("Title example", 12, 0, 0, 5.0, "Position should be between 0 and 11");
+            Add("Title example", 0, 2, 0, 5.0, "Placement should be between 0 and 1");
+            Add("Title example", 0, -1, 0, 5.0, "Placement should be between 0 and 1");
+            Add("Title example", 0, 0, -1, 5.0, "Orientation should be between 0 and 1");
+            Add("Title example", 0, 0, 2, 5.0, "Orientation should be between 0 and 1");
+            Add("Title example", 0, 0, 0, 4.9, "Text size should be between 5 and 140");
+            Add("Title example", 0, 0, 0, 140.1, "Text size should be between 5 and 140");
+        }
+    }
+
     public class TestGraphLegend
     {
         [Theory]
@@ -40,6 +65,25 @@ namespace UnitTestEngine
         public void CheckExceptions(string title, int position, string message)
         {
             var exception = Assert.Throws<ArgumentException>(() => new GraphLegend(title, position));
+            Assert.Equal(message, exception.Message);
+        }
+        [Theory]
+        [ClassData(typeof(TestGraphLegendAdvancedHelper))]
+        public void CorrectDataAdvanced(string title, int position, int placement, int orientation, double textSize, string toString)
+        {
+            GraphLegend testObject = new GraphLegend(title, position, placement, orientation, textSize);
+            Assert.Equal(title, testObject.Title);
+            Assert.Equal(position, testObject.Position);
+            Assert.Equal(placement, testObject.Placement);
+            Assert.Equal(orientation, testObject.Orientation);
+            Assert.Equal(textSize, testObject.TextSize);
+            Assert.Equal(toString, testObject.ToString());
+        }
+        [Theory]
+        [ClassData(typeof(TestGraphLegendAdvancedExceptionHelper))]
+        public void CheckAdvancedExceptions(string title, int position, int placement, int orientation, double textSize, string message)
+        {
+            var exception = Assert.Throws<ArgumentException>(() => new GraphLegend(title, position, placement, orientation, textSize));
             Assert.Equal(message, exception.Message);
         }
     }
