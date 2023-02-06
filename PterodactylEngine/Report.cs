1e17037c6f6166b6556831e83ade742364f74935
paireks <w.radaczynski@gmail.com>
2023-02-06 12:59:03 +0100

    Add auto-spacing

--- PterodactylEngine/Report.cs
+++ PterodactylEngine/Report.cs
@@ -1,61 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Text;
 
 namespace PterodactylEngine
 {
     public class Report
     {
-        private List<string> _reportParts;
-        private string _title;
-        private bool _tableOfContents;
-
-
-        public Report(List<string> reportParts, string title, bool tableOfContents)
+        public Report(List<string> reportParts, string title, bool tableOfContents, bool autoSpacing)
         {
             ReportParts = reportParts;
             TableOfContents = tableOfContents;
             Title = title;
+            AutoSpacing = autoSpacing;
         }
 
         public string Create()
         {
             StringBuilder report = new StringBuilder();
 
-            if (Title != "")
+            if (AutoSpacing)
             {
-                report.AppendLine("# " + Title);
-            }
+                if (Title != "")
+                {
+                    report.AppendLine("# " + Title);
+                    report.AppendLine();
+                }
 
-            if (TableOfContents)
+                if (TableOfContents)
+                {
+                    report.AppendLine("[TOC]");
+                    report.AppendLine();
+                }
+
+                foreach (string part in ReportParts)
+                {
+                    report.AppendLine(part);
+                    report.AppendLine();
+                }
+            }
+            else
             {
-                report.AppendLine("[TOC]");
-            }
+                if (Title != "")
+                {
+                    report.AppendLine("# " + Title);
+                }
 
-            foreach (string part in ReportParts)
-            {
-                report.AppendLine(part);
-            }
+                if (TableOfContents)
+                {
+                    report.AppendLine("[TOC]");
+                }
 
+                foreach (string part in ReportParts)
+                {
+                    report.AppendLine(part);
+                }
+            }
+            
             return report.ToString();
         }
 
-        public List<string> ReportParts
-        {
-            get { return _reportParts; }
-            set { _reportParts = value; }
-        }
+        public List<string> ReportParts { get; set; }
 
-        public string Title
-        {
-            get { return _title; }
-            set { _title = value; }
-        }
+        public string Title { get; set; }
 
-        public bool TableOfContents
-        {
-            get { return _tableOfContents; }
-            set { _tableOfContents = value; }
-        }
+        public bool TableOfContents { get; set; }
+
+        public bool AutoSpacing { get; set; }
     }
 }
--- PterodactylEngineTests/TestReport.cs
+++ PterodactylEngineTests/TestReport.cs
@@ -5,15 +5,20 @@ using Xunit;
 
 namespace UnitTestEngine
 {
-    public class TestReportHelper : TheoryData<List<string>, string, bool, string>
+    public class TestReportHelper : TheoryData<List<string>, string, bool, bool, string>
     {
         public TestReportHelper()
         {
-            Add(new List<string>{"Empty"}, "Test title", false,
+            Add(new List<string>{"Empty"}, "Test title", false, false,
                 "# Test title" + Environment.NewLine + "Empty" + Environment.NewLine);
-            Add(new List<string>{"Multiple", "lines"}, "", true,
+            Add(new List<string>{"Multiple", "lines"}, "", true, false,
                 "[TOC]" + Environment.NewLine + "Multiple" + Environment.NewLine + "lines"
                 + Environment.NewLine);
+            Add(new List<string>{"Empty"}, "Test title", false, true,
+                "# Test title" + Environment.NewLine + Environment.NewLine + "Empty" + Environment.NewLine + Environment.NewLine);
+            Add(new List<string>{"Multiple", "lines"}, "", true, true,
+                "[TOC]" + Environment.NewLine + Environment.NewLine + "Multiple" + Environment.NewLine + Environment.NewLine + "lines"
+                + Environment.NewLine + Environment.NewLine);
         }
     }
 
@@ -21,19 +26,22 @@ namespace UnitTestEngine
     {
         [Theory]
         [ClassData(typeof(TestReportHelper))]
-        public void CorrectData(List<string> reportParts, string title, bool tableOfContents, string expectedReport)
+        public void CorrectData(List<string> reportParts, string title, bool tableOfContents, bool autoSpacing,
+            string expected)
         {
-            Report testObject = new Report(reportParts, title, tableOfContents);
+            Report testObject = new Report(reportParts, title, tableOfContents, autoSpacing);
             Assert.Equal(reportParts, testObject.ReportParts);
             Assert.Equal(title, testObject.Title);
             Assert.Equal(tableOfContents, testObject.TableOfContents);
+            Assert.Equal(autoSpacing, testObject.AutoSpacing);
         }
 
         [Theory]
         [ClassData(typeof(TestReportHelper))]
-        public void CheckReportCreation(List<string> reportParts, string title, bool tableOfContents, string expected)
+        public void CheckReportCreation(List<string> reportParts, string title, bool tableOfContents, bool autoSpacing,
+            string expected)
         {
-            Report testObject = new Report(reportParts, title, tableOfContents);
+            Report testObject = new Report(reportParts, title, tableOfContents, autoSpacing);
             string actual = testObject.Create();
 
             Assert.Equal(expected, actual);
